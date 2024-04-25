c63022314123c9190119a4a2968b011a8e2b87cb
Harikrishnan17 <hyprgd2002@gmail.com>
2024-04-25 12:51:11 +0530

    Added changes

---
+++ src/static/data.js
@@ -0,0 +1,7 @@
+const carData = [
+  {
+    icon: "/icon-sedans.svg",
+    title: "Sedans",
+    desc: "Choose a sedan for its affordability and excellent fuel economy. Ideal for ruising in the city of on your next road trip.",
+  },
+];
