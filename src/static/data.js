4497c5879e50dc2c854474ac7ff90f5e57bb9844
Harikrishnan17 <hyprgd2002@gmail.com>
2024-04-26 12:51:30 +0530

    Added changes

--- src/static/data.js
+++ src/static/data.js
@@ -4,4 +4,9 @@ const carData = [
     title: "Sedans",
     desc: "Choose a sedan for its affordability and excellent fuel economy. Ideal for ruising in the city of on your next road trip.",
   },
+  {
+    icon: "/icon-suvs.svg",
+    title: "Suvs",
+    desc: "Take an SUV for its spacious interior, power, and versatility. Perfect for you next family vacation and off-road adventures.",
+  },
 ];
