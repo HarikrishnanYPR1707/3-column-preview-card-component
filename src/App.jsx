1437f8241b76b5eebfd1c1c448a02885ce7daf1a
Harikrishnan17 <hyprgd2002@gmail.com>
2024-04-28 12:52:13 +0530

    Added changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="text-3xl font-black underline">
-      <p className="font-spaceGrotesk">
-        Vite + TailwindCSS Template, with 3 fonts
-      </p>
-      <p className="font-familjenGrotesk">
-        Vite + TailwindCSS Template, with 3 fonts
-      </p>
-      <p className="font-spaceMono">
-        Vite + TailwindCSS Template, with 3 fonts
-      </p>
+    <div className="">
+      <img src="" alt="" />
     </div>
   );
 }
