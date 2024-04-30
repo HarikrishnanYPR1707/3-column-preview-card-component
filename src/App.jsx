20a4a57dbc8c1db72dcc955293bb2ef234bd8980
Harikrishnan17 <hyprgd2002@gmail.com>
2024-04-30 12:57:30 +0530

    Added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import { carData } from "./static/data";
 
 function App() {
   return (
     <div className="">
-      <img src="" alt="" />
+      {carData.map((item, index) => (
+        <article className="">
+          <img src={item.icon} alt="" />
+          <h1 className="">{item.title}</h1>
+          <p className="">{item.desc}</p>
+          <button className="">Learn More</button>
+        </article>
+      ))}
     </div>
   );
 }
