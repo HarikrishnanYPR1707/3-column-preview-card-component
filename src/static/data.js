8001e496ed2d8d8aea1989fa9dbfff1e9c44127a
Harikrishnan17 <hyprgd2002@gmail.com>
2024-05-01 12:59:52 +0530

    Added changes

--- src/static/data.js
+++ src/static/data.js
@@ -1,16 +1,19 @@
 export const carData = [
   {
     icon: "/icon-sedans.svg",
+    color: "#e28525",
     title: "Sedans",
     desc: "Choose a sedan for its affordability and excellent fuel economy. Ideal for ruising in the city of on your next road trip.",
   },
   {
     icon: "/icon-suvs.svg",
+    color: "#016972",
     title: "Suvs",
     desc: "Take an SUV for its spacious interior, power, and versatility. Perfect for you next family vacation and off-road adventures.",
   },
   {
     icon: "/icon-luxury.svg",
+    color: "#00403f",
     title: "Luxury",
     desc: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
   },
