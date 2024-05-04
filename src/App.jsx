09ed5a7ef8fda29dd624578c87fb51f0cb82beea
Harikrishnan17 <hyprgd2002@gmail.com>
2024-05-04 12:15:49 +0530

    Added vite template and added frontend mentor details

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-black underline">
+      <p className="font-spaceGrotesk">
+        Vite + TailwindCSS Template, with 3 fonts
+      </p>
+      <p className="font-familjenGrotesk">
+        Vite + TailwindCSS Template, with 3 fonts
+      </p>
+      <p className="font-spaceMono">
+        Vite + TailwindCSS Template, with 3 fonts
+      </p>
+    </div>
+  );
+}
+
+export default App;
